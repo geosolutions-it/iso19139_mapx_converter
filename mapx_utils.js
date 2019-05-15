378251961855d1d995faf04cfb0023f8282ea1eb
etj <etj@geo-solutions.it>
2019-05-15 18:33:55 +0200

    Add missing constants file.

---
+++ mapx_utils.js
@@ -0,0 +1,71 @@
+/* 
+ */
+
+module.exports = {
+        PARAM_LOG_INFO_NAME: "log_info",
+        PARAM_LOG_DEBUG_NAME: "log_debug",
+
+        PARAM_HOMEPAGE_TEMPLATE_NAME: "homepage_template",
+    
+        LANG_MAPPING_I2M: {
+            "eng": "en",
+            "fre": "fr",
+            "fra": "fr",
+            "spa": "es",
+            "rus": "ru",
+            "chi": "zh",
+            "zho": "zh",
+            "deu": "de",
+            "ger": "de",
+            "ben": "bn",
+            "per": "fa",
+            "fas": "fa",
+            "pus": "ps"
+        },
+        
+        LANG_MAPPING_M2I: {
+          "en" : "eng",
+          "fr" : "fra",
+          "es" : "spa",
+          "ru" : "rus",
+          "zh" : "chi",
+          "zh" : "zho",
+          "de" : "ger",
+          "bn" : "ben",
+          "fa" : "per",
+          "ps" : "pus"
+        },
+               
+        FREQ_MAPPING_I2M:  {
+            "continual":    "continual" ,
+            "daily":        "daily" ,
+            "weekly":       "weekly" ,
+            "fortnightly":  "fortnightly" ,
+            "monthly":      "monthly" ,
+            "quarterly":    "quarterly" ,
+            "biannually":   "biannually" ,
+            "annually":     "annually" ,
+            "asNeeded":     "as_needed" ,
+            "irregular" :   "irregular" ,
+            "notPlanned":   "not_planned" ,
+            "unknown":      "unknown"
+        },
+        FREQ_MAPPING_M2I:  {
+            "continual"   : "continual",
+            "daily"       : "daily",    
+            "weekly"      : "weekly",
+            "fortnightly" : "fortnightly",
+            "monthly"     : "monthly",
+            "quarterly"   : "quarterly",
+            "biannually"  : "biannually",
+            "annually"    : "annually",
+            "as_needed"   : "asNeeded",
+            "irregular"   : "irregular",
+            "not_planned" : "notPlanned",
+            "unknown"     : "unknown"
+        }
+};
+
+
+
+
